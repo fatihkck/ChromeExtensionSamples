740d1cdaf92fd99a1ad52ef666cc5b673c6f9845
Fatih Koçak <fatihkocak@hotmail.com>
2022-06-14 12:03:13 +0300

    Budget options

--- Tutorial 1/BudgetManager/assets/js/page/popup.js
+++ Tutorial 1/BudgetManager/assets/js/page/popup.js
@@ -1,5 +1,32 @@
-$(function(){
-    $('#name').keyup(function(){
-        $("#greet").html('Hello '+$('#name').val());
+$(function () {
+
+    chrome.storage.sync.get(['total','limit'],function(budget){
+        console.log(budget);
+        $('#total').html(budget.total);
+        $('#limit').html(budget.limit);
+    });
+
+    $('#spendAmount').click(function () {
+        chrome.storage.sync.get('total', function (budget) {
+            console.log("storage get");
+
+            var newTotal = 0;
+            console.log(budget);
+            if (budget.total) {
+                console.log("budget.total");
+                newTotal += parseInt(budget.total);
+            }
+            console.log(newTotal);
+            var amount = $('#amount').val();
+            if(amount){
+                console.log("amount");
+                newTotal +=parseInt(amount);
+            }
+
+            chrome.storage.sync.set({'total':newTotal});
+
+            $('#total').html(newTotal);
+            $('#amount').val('');
+        });
     });
 });
